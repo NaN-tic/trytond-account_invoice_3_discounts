f4ff92b090b1751d93c699c9c0ab367582184b74
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-20 11:41:34 +0200

    pep8

--- invoice.py
+++ invoice.py
@@ -52,20 +52,23 @@ class InvoiceLine:
         discount3 = self.discount3 or Decimal(0)
         self.discount = 1 - ((1 - discount1) * (1 - discount2) * (1 -
                 discount3))
-        digits = self.__class__.discount.digits[1]                   
+        digits = self.__class__.discount.digits[1]
         self.discount = self.discount.quantize(Decimal(str(10.0 ** -digits)))
         res = super(InvoiceLine, self).update_prices()
         res['discount'] = self.discount
         return res
 
-    @fields.depends('discount1', 'discount2', 'discount3', methods=['discount'])
+    @fields.depends('discount1', 'discount2', 'discount3',
+        methods=['discount'])
     def on_change_discount1(self):
         return self.update_prices()
 
-    @fields.depends('discount1', 'discount2', 'discount3', methods=['discount'])
+    @fields.depends('discount1', 'discount2', 'discount3',
+        methods=['discount'])
     def on_change_discount2(self):
         return self.update_prices()
 
-    @fields.depends('discount1', 'discount2', 'discount3', methods=['discount'])
+    @fields.depends('discount1', 'discount2', 'discount3',
+        methods=['discount'])
     def on_change_discount3(self):
         return self.update_prices()
