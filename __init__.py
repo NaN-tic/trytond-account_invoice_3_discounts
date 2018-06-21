14cffb79f250274d8a13f99baa72ae4436f3b8d8
Raimon Esteve <raimon@nan-tic.com>
2018-06-21 09:54:53 +0200

    Add commission extras depend

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import *
+from . import invoice
+from . import commission
 
 def register():
     Pool.register(
-        InvoiceLine,
+        invoice.InvoiceLine,
+        commission.Commission,
         module='account_invoice_3_discounts', type_='model')
